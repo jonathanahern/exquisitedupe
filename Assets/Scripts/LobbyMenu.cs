d257816515ccd88f5033775547762d4a699d2bbd
jonathanahern <jda409@nyu.edu>
2017-06-14 11:59:42 -0400

    About to start database mess up

---
+++ Assets/Scripts/LobbyMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyMenu : MonoBehaviour {
+
+	public GameObject mainButtons;
+	public GameObject turnBasedButtons;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TurnBasedClicked(){
+	
+		mainButtons.SetActive (false);
+		turnBasedButtons.SetActive (true);
+	
+	}
+}
